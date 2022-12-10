440ac4093eddb376a59656dc9529332bdf6ddf18
dolbilov <dolbilovkonstantin@gmail.com>
2022-12-10 22:49:38 +1000

    Переписал представления для вьюшек по выводу фильмов по рейтингам, объеденил три вьюшки в одну. Переписал вьюшку по выводу фильма по его тайтлу, добавил вывод ошибки, ксли такого фильма не нашлось. Добавил в каждую вьюшку вывод через jsonify

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import sqlite3
-from flask import Flask
-
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
@@ -15,24 +14,29 @@ def page_movie(title):
         f"""
         SELECT title, country, release_year, listed_in, description
         FROM netflix 
-        WHERE title = '{title}'
+        WHERE title LIKE '%{title}%'
         ORDER BY  release_year DESC
         """
     )
 
     result = cur.fetchone()
 
-    data = {
-        'title': result[0],
-        'country': result[1],
-        'release_year': result[2],
-        'genre': result[3],
-        'description': result[-1]
-    }
+    if result is None:
+        return jsonify(status=404)
+    else:
+        data = {
+            'title': result[0],
+            'country': result[1],
+            'release_year': result[2],
+            'genre': result[3],
+            'description': result[-1]
+        }
 
-    con.close()
+        con.close()
 
-    return data
+        return jsonify(data)
+
+
 
 
 @app.route('/movie/<int:year_from>/to/<int:year_to>')
@@ -62,22 +66,31 @@ def page_movie_between_years(year_from, year_to):
 
     con.close()
 
-    return data
+    return jsonify(data)
 
 
-@app.route('/rating/children')
-def page_movies_for_children():
+@app.route('/rating/<rating>')
+def page_movies_by_rating(rating):
     con = sqlite3.connect("netflix.db")
     cur = con.cursor()
 
-    cur.execute(
-        f"""
-        SELECT title, rating, description
-        FROM netflix
-        WHERE rating = 'G'
-        LIMIT 100
-        """
-    )
+    query = f"""
+            SELECT title, rating, description
+            FROM netflix
+    """
+
+    if rating == 'children':
+        query += "WHERE rating = 'G'"
+    elif rating == 'family':
+        query += "WHERE rating = 'G' OR rating = 'PG' OR rating = 'PG-13'"
+    elif rating == 'adult':
+        query += "WHERE rating = 'R' OR rating = 'NC-17'"
+    else:
+        return jsonify(status=404)
+
+    query += "LIMIT 100"
+
+    cur.execute(query)
 
     result = cur.fetchall()
 
@@ -92,67 +105,7 @@ def page_movies_for_children():
 
     con.close()
 
-    return data
-
-
-@app.route('/rating/family')
-def page_family_movies():
-    con = sqlite3.connect("netflix.db")
-    cur = con.cursor()
-
-    cur.execute(
-        f"""
-        SELECT title, rating, description
-        FROM netflix
-        WHERE rating = 'G' OR rating = 'PG' OR rating = 'PG-13'
-        LIMIT 100
-        """
-    )
-
-    result = cur.fetchall()
-
-    data = []
-    for row in result:
-        movie = {
-            'title': row[0],
-            'rating': row[1],
-            'description': row[2]
-        }
-        data.append(movie)
-
-    con.close()
-
-    return data
-
-
-@app.route('/rating/adult')
-def page_adult_movies():
-    con = sqlite3.connect("netflix.db")
-    cur = con.cursor()
-
-    cur.execute(
-        f"""
-        SELECT title, rating, description
-        FROM netflix
-        WHERE rating = 'R' OR rating = 'NC-17'
-        LIMIT 100
-        """
-    )
-
-    result = cur.fetchall()
-
-    data = []
-    for row in result:
-        movie = {
-            'title': row[0],
-            'rating': row[1],
-            'description': row[2]
-        }
-        data.append(movie)
-
-    con.close()
-
-    return data
+    return jsonify(data)
 
 
 @app.route('/genre/<genre>')
@@ -182,7 +135,7 @@ def page_by_genre(genre):
 
     con.close()
 
-    return data
+    return jsonify(data)
 
 
 app.run(host='0.0.0.0', port=800)
